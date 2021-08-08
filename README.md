a685b55ef682e2ba24a5285881a51db5f923c16e
Andres <fullbringdres@gmail.com>
2021-08-08 15:35:45 -0500

    Ready to upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# WS Chat
+
+I created this on codepen, I did it with EDteam's course about Ajax, Websockets and CSS, I hope you like it!
---
+++ dist/script.js
@@ -0,0 +1,35 @@
+"use strict";
+const url = "wss://echo.websocket.org";
+const conectar = document.getElementById("conectar");
+const desconectar = document.getElementById("desconectar");
+const submit = document.getElementById("submit");
+const message = document.getElementById("message");
+const user = document.getElementById("user");
+const chat = document.getElementById("chat");
+let ws = null;
+const setText = (data) => {
+    chat.insertAdjacentHTML("beforeend", `<p class="chat-message">${data}</p>`);
+};
+conectar.addEventListener('click', (e) => {
+    ws = new WebSocket(url);
+    ws.onopen = () => setText('Conectado');
+    ws.onclose = () => setText('Desconectado');
+    ws.onmessage = (e) => setText(e.data);
+    ws.onerror = (e) => console.log(e);
+});
+desconectar.addEventListener('click', () => {
+    ws ? ws.close() : console.log('No se ha conectado aun');
+});
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    const msg = message.value;
+    message.value = "";
+    ws.send(msg);
+});
+message.addEventListener('keyup', e => {
+    if (e.keyCode === 13) {
+        const msg = message.value;
+        message.value = "";
+        ws.send(msg);
+    }
+});
